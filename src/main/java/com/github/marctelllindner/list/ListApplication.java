e36acb5497603b8f8caaaad6b7b23bd6bf8616bb
Marc-Tell Lindner <tell96@freenet.de>
2021-05-08 11:43:27 +0200

    Fixed web-config

--- src/main/java/com/github/marctelllindner/list/ListApplication.java
+++ src/main/java/com/github/marctelllindner/list/ListApplication.java
@@ -3,7 +3,11 @@ package com.github.marctelllindner.list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@ComponentScan("com.github.marctelllindner.list.rest")
+@EnableWebMvc
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ListApplication {
 
--- src/main/java/com/github/marctelllindner/list/rest/Test.java
+++ src/main/java/com/github/marctelllindner/list/rest/Test.java
@@ -1,15 +1,21 @@
 package com.github.marctelllindner.list.rest;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class Test {
 
-    @GetMapping
-    public ResponseEntity<String> getTest() {
-        return ResponseEntity.ok("OK");
+    @GetMapping("/ok")
+    public String getOk() {
+        return "ok";
+    }
+
+    @GetMapping("/fine")
+    public String getFine() {
+        return "fine";
     }
 
 }
